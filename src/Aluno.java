b8a3a55c5a120a7d65fa795c99fe747d039fc64b
Julia <juliabarrozorp@gmail.com>
2025-05-20 22:29:16 -0300

    adicionando comentários

--- src/Aluno.java
+++ src/Aluno.java
@@ -1,12 +1,15 @@
 public class Aluno {
-    private String nome;
-    protected int matricula;
+    // declara atributos do tipo privado
+    private String nome; 
+    private int matricula; 
 
+    // construtor da Classe: inicializa o objeto
     public Aluno (String nome, int matricula) {
         this.nome = nome;
         this.matricula = matricula;
     }
 
+    // o metodo getter é publico e retorna uma String
     public String getNome() {
         return nome;
     }
@@ -14,13 +17,15 @@ public class Aluno {
         return matricula;
     }
 
+    // o metodo setter é publico e não retorna nada, ele recebe um parametro do tipo String
     public void setNome(String nome) {
-        this.nome = nome;
+        this.nome = nome; // substitui o atributo nome pelo parametro nome
     }
     public void setMatricula(int matricula) {
         this.matricula = matricula;
     }
 
+    // esse metodo revela informações retornadas nos getters.
     public void exibirInformações() {
         System.out.println("Nome: " + nome + "\nMatricula: " + matricula);
     }
--- src/App.java
+++ src/App.java
@@ -1,12 +1,19 @@
 public class App {
     public static void main(String[] args) throws Exception {
+
+        // cria uma instancia de Aluno
         Aluno aluno1 = new Aluno("Carol", 20527638);
 
+        // chama o metodo que exibe informações
+        System.out.println("- Informações -");
         aluno1.exibirInformações();
 
+        // chama os setters estabelecendo parametros e muda as informações estabelecidas inicialmente
         aluno1.setNome("Mariana");
         aluno1.setMatricula(12345678);
 
+        // chama o metodo que exibe informações novamente
+        System.out.println("- Informações Atualizadas -");
         aluno1.exibirInformações();
     }
 }
